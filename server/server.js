f0a62221927d0f001498d7230adf4d46419e599c
Rato do Arroz <131390510+Ratatouille-87@users.noreply.github.com>
2024-04-11 21:46:24 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,16 +2,10 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const { exec } = require('child_process');
+const convertFileToPDF = require('./conversor');
 
 const app = express();
 const PORT = 3000;
-
-// Verificar se o diretório de uploads existe, se não, criar
-const uploadDirectory = path.join(__dirname, 'uploads');
-if (!fs.existsSync(uploadDirectory)) {
-    fs.mkdirSync(uploadDirectory);
-}
 
 // Configuração do multer para armazenar os arquivos enviados
 const storage = multer.diskStorage({
@@ -26,25 +20,28 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage, limits: { fileSize: 10 * 1024 * 1024 } }); // Limite de 10MB para o tamanho do arquivo
 
 // Rota para lidar com o upload de arquivos
-app.post('/conversao', upload.single('file'), (req, res) => {
+app.post('/conversao', upload.single('file'), async (req, res) => {
     if (!req.file) {
         return res.status(400).send('Nenhum arquivo enviado.');
     }
 
-    // Executa o conversor.js para processar o arquivo enviado
-    exec('node conversor.js ' + req.file.path, (error, stdout, stderr) => {
-        if (error) {
-            console.error('Erro ao executar o conversor:', error);
-            res.status(500).send('Erro ao processar o arquivo.');
-            return;
-        }
+    const filePath = req.file.path;
+    const outputPdfPath = `${filePath}.pdf`;
 
-        // Exclui o arquivo enviado após a conversão
-        fs.unlinkSync(req.file.path);
+    try {
+        // Converter o arquivo para PDF
+        await convertFileToPDF(filePath, outputPdfPath);
 
-        // Redireciona de volta para o index.html com mensagem de confirmação
-        res.redirect('/?mensagem=Arquivo convertido com sucesso.');
-    });
+        // Enviar o arquivo PDF convertido como resposta
+        res.download(outputPdfPath, () => {
+            // Excluir arquivos temporários após o download
+            fs.unlinkSync(filePath);
+            fs.unlinkSync(outputPdfPath);
+        });
+    } catch (error) {
+        console.error('Erro ao converter o arquivo para PDF:', error);
+        res.status(500).send('Erro ao converter o arquivo para PDF.');
+    }
 });
 
 // Servir os arquivos estáticos na pasta 'uploads'
