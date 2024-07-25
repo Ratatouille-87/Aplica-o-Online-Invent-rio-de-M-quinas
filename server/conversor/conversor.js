c3bc30501c020b4eb34155d4124add97d0ec71af
Rato do Arroz <131390510+Ratatouille-87@users.noreply.github.com>
2024-07-25 13:57:06 -0300

    Update conversor.js

--- server/conversor/conversor.js
+++ server/conversor/conversor.js
@@ -9,15 +9,13 @@ async function convertToPDF(inputFilePath, outputPdfPath) {
             throw new Error('Arquivo de entrada não encontrado');
         }
 
-        // Detecta o encoding do arquivo
-        const encoding = chardet.detectFileSync(inputFilePath);
-        if (encoding !== 'UTF-8') {
-            throw new Error(`Encoding incompatível: ${encoding}`);
+        // Lê o conteúdo do arquivo de entrada com encoding UTF-8
+        let originalContent;
+        try {
+            originalContent = await fs.promises.readFile(inputFilePath, { encoding: 'utf8' });
+        } catch (err) {
+            throw new Error('Erro ao ler o arquivo de entrada. Verifique se o arquivo está no formato e encoding corretos.');
         }
-
-        // Lê o conteúdo do arquivo de entrada com encoding detectado
-        const originalContent = await fs.promises.readFile(inputFilePath, { encoding: 'utf8' });
-
         // Cria um novo documento PDF
         const doc = new PDFDocument();
 
