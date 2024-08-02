49e69d842cd8bb6687e6e0b38dadbaed61b44d1f
Rato do Arroz <131390510+Ratatouille-87@users.noreply.github.com>
2024-08-01 21:00:13 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,26 +1,31 @@
-const express = require('express');
-const cors = require('cors');
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
-const { convertToPDF } = require('./conversor/conversor.js');
+import express from 'express';
+import cors from 'cors';
+import multer from 'multer';
+import { join, resolve, extname, basename, dirname } from 'path';
+import { existsSync, mkdirSync, unlinkSync } from 'fs';
+import { Worker } from 'worker_threads';
+import { convertToPDF } from './conversor/conversor.js';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(join(__dirname, 'public')));
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        const uploadDir = path.resolve(__dirname, 'uploads');
-        if (!fs.existsSync(uploadDir)) {
-            fs.mkdirSync(uploadDir);
+        const uploadDir = resolve(__dirname, 'uploads');
+        if (!existsSync(uploadDir)) {
+            mkdirSync(uploadDir);
         }
         cb(null, uploadDir);
     },
     filename: function (req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname));
+        cb(null, Date.now() + extname(file.originalname));
     }
 });
 
@@ -31,21 +36,44 @@ app.post('/upload', upload.single('file'), async (req, res) => {
         return res.status(400).send('Nenhum arquivo selecionado. Por favor, selecione um arquivo para continuar.');
     }
     const filePath = req.file.path;
-    const originalName = path.basename(req.file.originalname, path.extname(req.file.originalname));
-    const outputPdfPath = path.join(path.dirname(filePath), `${originalName}.pdf`);
+    const originalName = basename(req.file.originalname, extname(req.file.originalname));
+    const outputPdfPath = join(dirname(filePath), `${originalName}.pdf`);
 
     try {
         console.log(`Iniciando conversão para o arquivo: ${filePath}`);
-        await convertToPDF(filePath, outputPdfPath);
-        console.log(`Conversão concluída. Iniciando download do arquivo: ${outputPdfPath}`);
-        res.download(outputPdfPath, (err) => {
-            if (err) {
-                console.error(`Erro ao fazer o download do arquivo convertido: ${err}`);
-                res.status(500).send('Erro ao fazer o download do arquivo convertido.');
-            } else {
-                fs.unlinkSync(filePath); // Remove o arquivo de entrada após o download
-                fs.unlinkSync(outputPdfPath); // Remove o arquivo PDF após o download
-                console.log(`Arquivos removidos: ${filePath}, ${outputPdfPath}`);
+
+        const worker = new Worker(new URL('./workers/worker.js', import.meta.url));
+        worker.postMessage(filePath);
+
+        worker.on('message', async (result) => {
+            try {
+                await convertToPDF(result, outputPdfPath);
+                console.log(`Conversão concluída. Iniciando download do arquivo: ${outputPdfPath}`);
+                res.download(outputPdfPath, (err) => {
+                    if (err) {
+                        console.error(`Erro ao fazer o download do arquivo convertido: ${err}`);
+                        res.status(500).send('Erro ao fazer o download do arquivo convertido.');
+                    } else {
+                        unlinkSync(filePath);
+                        unlinkSync(outputPdfPath);
+                        console.log(`Arquivos removidos: ${filePath}, ${outputPdfPath}`);
+                    }
+                });
+            } catch (error) {
+                console.error(`Erro ao processar o arquivo: ${error}`);
+                res.status(500).send(`Erro ao processar o arquivo: ${error.message}`);
+            }
+        });
+
+        worker.on('error', (error) => {
+            console.error(`Erro no worker: ${error}`);
+            res.status(500).send(`Erro no worker: ${error.message}`);
+        });
+
+        worker.on('exit', (code) => {
+            if (code !== 0) {
+                console.error(`Worker finalizado com código: ${code}`);
+                res.status(500).send(`Worker finalizado com código: ${code}`);
             }
         });
     } catch (error) {
